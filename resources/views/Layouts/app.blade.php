780ee3d52724fa96b304e74a2672f044e4d79c0d
Girl100 <lizzaputri27@gmail.com>
2025-06-16 13:35:49 +0700

    inisialisasi proyek

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -1,17 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Latihan Laravel</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    @include('layouts.header')
+    <title>@yield('title')</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="widht=device-widht, initial-scale=1.0"/>
 
-    <div class="container mt-4">
-        @yield('content')
-    </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    @include('partials.header')
+
+
+    @yield('content')    
+
     @include('partials.footer')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,28 @@ use App\Http\Controllers\AdminController;
 //    return view('welcome');
 // });
 
-Route::get('/', [ProductController::class, 'index']);
-Route::get('/product/{id}', [ProductController::class, 'show']);
-Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
-Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
-Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
-Route::post('/contact/store', [ContactController::class, 'store'])->name('contact.store');
-Route::get('/admin/create', [AdminController::class, 'create'])->name('admin.create');
-Route::post('/admin/store', [AdminController::class, 'store'])->name('admin.store');
-Route::get('/home', function () {
+// Route::get('/', [ProductController::class, 'index']);
+// Route::get('/product/{id}', [ProductController::class, 'show']);
+
+// Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+// Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+
+// Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
+// Route::post('/contact/store', [ContactController::class, 'store'])->name('contact.store');
+
+// Route::get('/admin/create', [AdminController::class, 'create'])->name('admin.create');
+// Route::post('/admin/store', [AdminController::class, 'store'])->name('admin.store');
+
+Route::get('/', function () {
     return view('home');
 });
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
-Route::get('/gallery', function () {
-    return view('gallery');
-});
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+// Route::get('/gallery', function () {
+//     return view('gallery');
+// });
